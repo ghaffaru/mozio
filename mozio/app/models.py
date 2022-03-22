de9725f6ddd6c9bab976a01fad19f6000497c277
Ghaffaru Mudashiru <ghaffarumudashiru@Ghaffarus-MacBook-Pro.local>
2022-03-22 14:55:23 +0000

    clean up codebase

--- mozio/app/models.py
+++ mozio/app/models.py
@@ -17,7 +17,3 @@ class Polygon(models.Model):
     price = models.DecimalField(max_digits=100, decimal_places=2)
     geo = PointField()
     createdAt = models.DateTimeField(auto_now_add=True)
-
-    # @property
-    # def lat(self):
-    #     return self.geo.po
